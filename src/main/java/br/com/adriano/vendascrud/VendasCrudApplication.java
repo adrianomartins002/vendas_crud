92c8ac166e4006af39549f68ed243f7a7d2df6f1
Adriano <adrianomartins002@gmail.com>
2019-10-29 19:46:32 -0300

    feat(iniciar projeto): commit inicial do projeto criado com springboot

---
+++ src/main/java/br/com/adriano/vendascrud/VendasCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.adriano.vendascrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/adriano/vendascrud/VendasCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adriano.vendascrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
