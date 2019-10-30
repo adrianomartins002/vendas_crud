2ad77f82ea24448af5afa07b1fe431c6685cd2f1
Adriano <adrianomartins002@gmail.com>
2019-10-30 07:17:12 -0300

    feat(adicionar services e repository de categoria)

---
+++ src/main/java/br/com/adriano/vendascrud/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package br.com.adriano.vendascrud.services;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.adriano.vendascrud.domain.Categoria;
+import br.com.adriano.vendascrud.repository.CategoriaRepository;
+
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
