e2d3c13c15286bc4f552ef5cf0933558c887a908
Adriano <adrianomartins002@gmail.com>
2019-10-31 15:09:59 -0300

    feat(criar operacao de instancias para testsee)

--- src/main/java/br/com/adriano/vendascrud/VendasCrudApplication.java
+++ src/main/java/br/com/adriano/vendascrud/VendasCrudApplication.java
@@ -1,13 +1,32 @@
 package br.com.adriano.vendascrud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class VendasCrudApplication {
+import br.com.adriano.vendascrud.domain.Categoria;
+import br.com.adriano.vendascrud.repository.CategoriaRepository;
+import br.com.adriano.vendascrud.resources.CategoriaResource;
 
+@SpringBootApplication
+public class VendasCrudApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasCrudApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(cat1);
+		categoriaRepository.save(cat2);
+		
 	}
 
 }
