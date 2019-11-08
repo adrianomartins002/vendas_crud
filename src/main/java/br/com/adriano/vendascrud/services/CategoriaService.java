c528461de375796514cd470ec2ef833e5b7b4634
Adriano <adrianomartins002@gmail.com>
2019-11-08 20:30:28 -0300

    feat(adicionar endpoint de atualizacao de categoria)

--- src/main/java/br/com/adriano/vendascrud/services/CategoriaService.java
+++ src/main/java/br/com/adriano/vendascrud/services/CategoriaService.java
@@ -4,7 +4,12 @@ package br.com.adriano.vendascrud.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.adriano.vendascrud.domain.Categoria;
 import br.com.adriano.vendascrud.repository.CategoriaRepository;
@@ -22,5 +27,16 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
 	}
 }
